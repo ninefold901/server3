51f8129f13cfe728944700e9f4d5495073dbca9e
ninefold901 <becky51@126.com>
2020-06-17 21:47:10 +0800

    fix: rollup all

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,16 @@
 import { terser } from "rollup-plugin-terser";
 import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
     input: 'index.ts',
     output: {
-        file: 'dist/server.js',
-        format: 'cjs'
+        // file: 'dist/server.js',
+        format: 'cjs',
+        dir: 'dist'
     },
     plugins: [
+        commonjs(),
         typescript({
             module: 'es6',
             allowJs: true
